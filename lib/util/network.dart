e5e7eaed2392717a8bf793d6729883d4bb5bd90b
Jason Sebastian <sebastianj@garena.com>
2022-08-26 20:17:23 +0800

    Improve network.dart

--- lib/util/network.dart
+++ lib/util/network.dart
@@ -2,15 +2,16 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 
-const baseUrl = 'http://datamall2.mytransport.sg/ltaodataservice';
-
 Future<Response> getArrivalsFromApi(busStopCode) =>
-    request('$baseUrl/BusArrivalv2?BusStopCode=$busStopCode');
+    request('BusArrivalv2?BusStopCode=$busStopCode');
 
-Future<Response> getArrivalFromApi(busStopCode, serviceNo) => request(
-    '$baseUrl/BusArrivalv2?BusStopCode=$busStopCode&ServiceNo=$serviceNo');
+Future<Response> getArrivalFromApi(busStopCode, serviceNo) =>
+    request('BusArrivalv2?BusStopCode=$busStopCode&ServiceNo=$serviceNo');
 
-Future<Response> request(url) => http.get(Uri.parse(url), headers: {
-      'AccountKey': dotenv.env['API_KEY']!,
-      'Accept': 'application/json',
-    });
+Future<Response> request(endpoint) => http.get(
+      Uri.parse('http://datamall2.mytransport.sg/ltaodataservice/$endpoint'),
+      headers: {
+        'AccountKey': dotenv.env['API_KEY']!,
+        'Accept': 'application/json',
+      },
+    );
